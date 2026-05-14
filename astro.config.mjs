f53269664410bdc9697c9b2b1f279356418007ca
Azat Israilov <azat@azatisrail.org>
2026-05-14 13:34:36 +0600

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import keystatic from '@keystatic/astro';
-import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
-
-export default defineConfig({
-  output: 'hybrid',
-  adapter: cloudflare(),
-  integrations: [mdx(), keystatic(), react()],
-});
+import { defineConfig } from 'astro/config'; 
+import mdx from '@astrojs/mdx'; 
+import keystatic from '@keystatic/astro'; 
+import react from '@astrojs/react'; 
+import cloudflare from '@astrojs/cloudflare'; 
+import sitemap from '@astrojs/sitemap'; 
+ 
+export default defineConfig({ 
+  site: 'https://azatisrail.org', 
+  output: 'hybrid', 
+  adapter: cloudflare(), 
+  integrations: [mdx(), keystatic(), react(), sitemap()], 
+}); 
