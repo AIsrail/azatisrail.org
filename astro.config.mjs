f20f03282950566c2537deb834dd24f4902e3152
Azat Israilov <azat@azatisrail.org>
2026-05-14 13:39:06 +0600

    fix sitemap routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
-import { defineConfig } from 'astro/config'; 
-import mdx from '@astrojs/mdx'; 
-import keystatic from '@keystatic/astro'; 
-import react from '@astrojs/react'; 
-import cloudflare from '@astrojs/cloudflare'; 
-import sitemap from '@astrojs/sitemap'; 
- 
-export default defineConfig({ 
-  site: 'https://azatisrail.org', 
-  output: 'hybrid', 
-  adapter: cloudflare(), 
-  integrations: [mdx(), keystatic(), react(), sitemap()], 
-}); 
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import keystatic from '@keystatic/astro';
+import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://azatisrail.org',
+  output: 'hybrid',
+  adapter: cloudflare({
+    routes: {
+      extend: {
+        exclude: [
+          { pattern: '/sitemap-index.xml' },
+          { pattern: '/sitemap-0.xml' },
+          { pattern: '/robots.txt' },
+        ]
+      }
+    }
+  }),
+  integrations: [mdx(), keystatic(), react(), sitemap()],
+});
